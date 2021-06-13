4c0cfb842688e133fdc85eb84e4e05ffa87ef13b
wfy <kaliawng@163.com>
2021-06-13 10:43:47 +0800

    const commint

---
+++ Source/basics/04const.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#define day 7
+//变量 方便管理内存
+int main(){
+   
+  cout<<day<<endl;
+  const int month = 30;
+  cout<<month<<endl;
+  system("pause");
+  return 0;
+}
